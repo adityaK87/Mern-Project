a8398d490a719cc9af9fb7aad8bbb339637b91cc
Aditya Kumar <adityapal1409@gmail.com>
2023-08-24 16:01:27 +0530

    creating virtual for the project

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
+const { createHmac } = await import("node:crypto");
+const uuid4 = require("uuid/v4");
 
 const userSchema = new Schema({
 	name: {
@@ -24,10 +25,9 @@ const userSchema = new Schema({
 		type: String,
 		trim: true,
 	},
-	//TODO: COME BACK HERE
-	password: {
+	encry_password: {
 		type: String,
-		trim: true,
+		required: true,
 	},
 	salt: String,
 	role: {
@@ -39,5 +39,33 @@ const userSchema = new Schema({
 		default: [],
 	},
 });
+
+userSchema
+	.virtual("password")
+	.set(function (password) {
+		this._password = password;
+		this.salt = uuid4();
+		this.encry_password = this.securePassword(password);
+	})
+	.get(function () {
+		return this._password;
+	});
+
+userSchema.method = {
+	authenticate: function (plainPassword) {
+		return this.securePassword(plainPassword) === this.encry_password;
+	},
+	securePassword: function (plainPassword) {
+		if (!password) return "";
+		try {
+			// https://nodejs.org/api/crypto.html
+			return createHmac("sha256", this.salt)
+				.update(plainPassword)
+				.digest("hex");
+		} catch (error) {
+			return "";
+		}
+	},
+};
 
 module.exports = mongoose.model("User", userSchema);
