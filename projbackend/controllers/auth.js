7b8ec7a6ed085cabc52343d5344c667b958b3758
Aditya Kumar <adityapal1409@gmail.com>
2023-09-05 12:57:04 +0530

    post request sent

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,3 +1,9 @@
+exports.signup = (req, res) => {
+	res.json({
+		message: "sign up route works",
+	});
+};
+
 exports.signout = (req, res) => {
 	res.json({
 		message: "user has been signed out",
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { signout } = require("../controllers/auth");
+const { signout, signup } = require("../controllers/auth");
 
+router.post("/signup", signup);
 router.get("/signout", signout); // signout is coming from the auth controller
 
 //all the things have been exported wth router.
