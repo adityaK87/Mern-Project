d4d77d86625e4d2e3ca4bc013d9f967daab69167
Aditya Kumar <adityapal1409@gmail.com>
2023-09-05 12:31:35 +0530

    create auth controler

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { signout } = require("../controllers/auth");
 
-router.get("/signout", (req, res) => {
-	res.send("User has been signed out");
-});
+router.get("/signout", signout); // signout is coming from the auth controller
 
 //all the things have been exported wth router.
 module.exports = router;
