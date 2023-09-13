bf481550b817fba29f0917d556e048ebd8680b2f
Aditya Kumar <adityapal1409@gmail.com>
2023-09-13 21:06:27 +0530

    create a userPurchaseList method

--- projbackend/controllers/user.js
+++ projbackend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const Order = require("../models/order");
 
 exports.getUserId = (req, res, next, id) => {
 	User.findById(id).exec((err, user) => {
@@ -19,13 +20,35 @@ exports.getUser = (req, res) => {
 };
 
 //Get All the Users
-exports.getAllUsers = (req, res) => {
-	User.find().exec((err, users) => {
-		if (err || !users) {
-			return res.status(404).json({
-				error: " No user Found In DB",
-			});
+exports.updateUser = (req, res) => {
+	User.findByIdAndUpdate(
+		{ _id: req.profile._id },
+		{ $set: req.body },
+		{ new: true, useFindAndModify: false },
+		(err, user) => {
+			if (err) {
+				return res.status(400).json({
+					error: "You are not allowed to edit this",
+				});
+			}
+			user.salt = undefined;
+			user.encry_password = undefined;
+			res.json(user);
 		}
-		res.status(200).json(users);
-	});
+	);
+};
+
+exports.userPurchaseList = (req, res) => {
+	Order.find({
+		user: req.profile._id,
+	})
+		.poulate("user", "_id name")
+		.exec((err, order) => {
+			if (err) {
+				return res
+					.status(400)
+					.json({ error: "No orderin this account" });
+			}
+			return res.json(order);
+		});
 };
--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserId, getUser, getAllUsers } = require("../controllers/user");
+const {
+	getUserId,
+	getUser,
+	updateUser,
+	userPurchaseList,
+} = require("../controllers/user");
+
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
 router.param("userId", getUserId);
 
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
 
-router.get("/users", getAllUsers);
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser);
+router.get(
+	"/orders/user/:userId",
+	isSignedIn,
+	isAuthenticated,
+	userPurchaseList
+);
 
 module.exports = router;
