3ebfe39b6495b69945d2c843a7c703366adb7f87
Aditya Kumar <adityapal1409@gmail.com>
2023-09-13 11:51:08 +0530

    modify user route

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user");
+const { getUserId, getUser } = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+
+router.param("userId", getUserId);
+
+router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+
 module.exports = router;
