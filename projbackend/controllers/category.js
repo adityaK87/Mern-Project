e60dced1fbd9f6b278eb658c39e8a60d4d3a4b8a
Aditya Kumar <adityapal1409@gmail.com>
2023-09-14 11:23:33 +0530

    creating category and saving it

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -11,3 +11,17 @@ exports.getCategoryById = (req, res, next, id) => {
 	});
 	next();
 };
+
+exports.createCategory = (req, res) => {
+	let category = new Category(req.body);
+	category.save((err, category) => {
+		if (err) {
+			return res.status(400).json({
+				error: "NOT able to save category in DB",
+			});
+		}
+		res.json({
+			category,
+		});
+	});
+};
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById } = require("../controllers/category");
+const { getCategoryById, createCategory } = require("../controllers/category");
 const { isAdmin, isAuthenticated, isSignedIn } = require("../controllers/auth");
 const { getUserId } = require("../controllers/user"); //it just populates the parameters
 
+//Params
 router.param("userId", getUserId);
 router.param("categoryId", getCategoryById);
+
+//Actual Routes
+router.post(
+	"/category/create/:userId",
+	isSignedIn,
+	isAuthenticated,
+	isAdmin,
+	createCategory
+);
 
 module.exports = router;
