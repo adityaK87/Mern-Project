d65562ec03b3fbd792ab0384e1d13ae1b6f007b3
Aditya Kumar <adityapal1409@gmail.com>
2023-09-19 12:39:07 +0530

    create order function

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -5,12 +5,21 @@ const { isAuthenticated, isSignedIn, isAdmin } = require("../controllers/auth");
 const { getUserId, pushOrderInPurchaseList } = require("../controllers/user");
 const { updateStock } = require("../controllers/product");
 
-const { getOrderbyId } = require("../controllers/order");
+const { getOrderbyId, createOrder } = require("../controllers/order");
 
 //params
 router.param("userId", getUserId);
 router.param("orderId", getOrderbyId);
 
 //All routes
+//create
+router.post(
+	"/order/create/:userId",
+	isSignedIn,
+	isAuthenticated,
+	pushOrderInPurchaseList,
+	updateStock,
+	createOrder
+);
 
 module.exports = router;
