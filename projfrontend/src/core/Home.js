31b3684ef050bb32090791825d6657df724d6b93
Aditya Kumar <adityapal1409@gmail.com>
2023-09-27 13:36:14 +0530

    Take full advantage of Base component

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "../styles.css";
 import { API } from "../backend";
+import Base from "./Base";
 
 const Home = () => {
 	console.log("API IS ", API);
-	return <h1 className='text-white'>This is the Home component</h1>;
+	return (
+		<Base title="Home Page">
+			<h1 className="text-white">This is the Home component</h1>
+			<div className="row">
+				<div className="col-4">
+					<button className="btn btn-success">TEST</button>
+				</div>
+
+				<div className="col-4">
+					<button className="btn btn-success">TEST</button>
+				</div>
+				<div className="col-4">
+					<button className="btn btn-success">TEST</button>
+				</div>
+			</div>
+		</Base>
+	);
 };
 
 export default Home;
