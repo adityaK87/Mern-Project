7cdb39c7c1aa8aec77e30280cea7ac34063625bf
Aditya Kumar <adityapal1409@gmail.com>
2023-09-28 22:33:34 +0530

    add styling to navbar

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,39 +1,85 @@
 import React from "react";
-import { Link } from "react-router-dom";
-//TODO: ADD withRouter functionality
+import { Link, useLocation } from "react-router-dom";
 
-const Menu = () => {
+//This is a higher order function
+const withLocation = (Menu) => (props) => {
+	const location = useLocation();
+	return true ? <Menu {...props} location={location} /> : "";
+};
+
+const currentTab = (location, path) => {
+	if (location === path) {
+		return {
+			color: "#ffffff",
+		};
+	} else {
+		return {
+			color: "#d1d1d1",
+		};
+	}
+};
+
+const Menu = ({ location }) => {
 	return (
 		<div>
 			<ul className="nav nav-tabs bg-dark">
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
+					<Link
+						style={currentTab(location.pathname, "/")}
+						className="nav-link"
+						to="/">
 						Home
 					</Link>
 				</li>
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
+					<Link
+						style={currentTab(location.pathname, "/cart")}
+						className="nav-link"
+						to="/cart">
 						Cart
 					</Link>
 				</li>
 
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
+					<Link
+						style={currentTab(location.pathname, "/user/dashboard")}
+						className="nav-link"
+						to="/user/dashboard">
 						Dashboard
 					</Link>
 				</li>
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
+					<Link
+						style={currentTab(
+							location.pathname,
+							"/admin/dashboard"
+						)}
+						className="nav-link"
+						to="/admin/dashboard">
+						A. Dashboard
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link
+						className="nav-link"
+						style={currentTab(location.pathname, "/signup")}
+						to="/signup">
 						Signup
 					</Link>
 				</li>
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
-						Sign in
+					<Link
+						style={currentTab(location.pathname, "/signin")}
+						className="nav-link"
+						to="/signin">
+						Signin
 					</Link>
 				</li>
 				<li className="nav-item">
-					<Link className="nav-link" to="/">
+					<Link
+						style={currentTab(location.pathname, "/signout")}
+						className="nav-link"
+						to="/signout">
 						signout
 					</Link>
 				</li>
@@ -42,4 +88,4 @@ const Menu = () => {
 	);
 };
 
-export default Menu;
+export default withLocation(Menu);
