337d4ba5f83ffb4adf94bc1773e9e62300be68ff
Aditya Kumar <adityapal1409@gmail.com>
2023-09-29 13:00:08 +0530

    create signup and signin form

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -3,9 +3,37 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+	const signinForm = () => {
+		return (
+			<div className="row">
+				<div className="col-6 offset-sm-3 text-left">
+					<form action="">
+						<div className="form-group">
+							<label htmlFor="" className="text-light">
+								Email
+							</label>
+							<input type="email" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="" className="text-light">
+								Password
+							</label>
+							<input type="password" className="form-control" />
+						</div>
+						<button
+							type="submit"
+							className="btn btn-success btn-block w-100 mt-3">
+							Submit
+						</button>
+					</form>
+				</div>
+			</div>
+		);
+	};
+
 	return (
 		<Base title="Sign In page" description="A page for user to sign in!">
-			<h1>Signin works</h1>
+			{signinForm()}
 		</Base>
 	);
 };
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -3,9 +3,43 @@ import Base from "../core/Base";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+	const signupForm = () => {
+		return (
+			<div className="row">
+				<div className="col-6 offset-sm-3 text-left">
+					<form action="">
+						<div className="form-group">
+							<label htmlFor="" className="text-light">
+								Name
+							</label>
+							<input type="text" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="" className="text-light">
+								Email
+							</label>
+							<input type="email" className="form-control" />
+						</div>
+						<div className="form-group">
+							<label htmlFor="" className="text-light">
+								Password
+							</label>
+							<input type="password" className="form-control" />
+						</div>
+						<button
+							type="submit"
+							className="btn btn-success btn-block w-100 mt-3">
+							Submit
+						</button>
+					</form>
+				</div>
+			</div>
+		);
+	};
+
 	return (
 		<Base title="Sign Up page" description="A page for user to sign up!">
-			<h1>Signup works</h1>
+			{signupForm()}
 		</Base>
 	);
 };
